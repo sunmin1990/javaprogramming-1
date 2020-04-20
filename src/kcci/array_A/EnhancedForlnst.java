d9244618aff570ab2f10a492f046dd76434ec8dc
kccistc <kccistc@DESKTOP-SV7J47R>
2020-04-20 16:58:39 +0900

    2020-04-20 EnhancedForInst

--- src/kcci/array_A/EnhancedForlnst.java
+++ src/kcci/array_A/EnhancedForlnst.java
@@ -1,10 +1,10 @@
 package kcci.array_A;
 
-class Box {
+class Box33 {
 	private String contents;
 	private int boxNum;
 	
-	Box(int num, String cont){
+	Box33(int num, String cont){
 		boxNum = num;
 		contents = cont;
 	}
@@ -18,12 +18,12 @@ class Box {
 
 public class EnhancedForlnst {
 	public static void main(String[]args) {
-		Box[] ar = new Box[5];
-		ar[0] = new Box(101, "Coffee");
-		ar[1] = new Box(202, "Computer");
-		ar[2] = new Box(303, "Apple");
-		ar[3] = new Box(404, "Dress");
-		ar[4] = new Box(505, "Fairy-tale book");
+		Box33[] ar = new Box33[5];
+		ar[0] = new Box33(101,"Coffee");
+		ar[1] = new Box33(202,"Computer");
+		ar[2] = new Box33(303,"Apple");
+		ar[3] = new Box33(404,"Dress");
+		ar[4] = new Box33(505,"Fairy-tale book");
 	}
 
 }
