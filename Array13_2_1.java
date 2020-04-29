5a5b98cf3c58d0ad8df95f65230dd4719c71adcb
sunmin1990 <63627305+sunmin1990@users.noreply.github.com>
2020-04-29 16:36:57 +0900

    연습문제13-2-1 오류

---
+++ Array13_2_1.java
@@ -0,0 +1,22 @@
+package Study_workshop;
+
+public class Array13_2_1 {
+	
+	public static void addOneDArr(int[] arr, int add) {
+		for(int i =0; i<arr.length; i++)
+			arr[i] +=add;
+	}
+	
+	public static void addTwoDArr(int[][] arr, int add) {
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j; j<arr[i].length; j++)
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(addTwoDArr(arr));
+	}
+
+}
